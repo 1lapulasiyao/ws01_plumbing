e8e4159864f5aed5fdf7e107f2c1f933e0ab7218
1lapulasiyao <513210591@qq.com>
2026-06-26 11:15:45 +0800

    third save

---
+++ build/py01_topic/colcon_command_prefix_setup_py.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/abc/ws01_plumbing/install/base_interface/share/base_interface/package.sh"
--- src/cpp01_topic/src/talker_str_cpp.cpp
+++ src/cpp01_topic/src/talker_str_cpp.cpp
@@ -1,8 +1,19 @@
 #include "rclcpp/rclcpp.hpp"
 
+class Talker: public rclcpp::Node
+{
+public:
+    Talker(): Node("talker_str_cpp")
+    {
+        RCLCPP_INFO(this->get_logger(), "CPP_STR发布者启动成功");
+    }
+};
+
 int main(int argc, char *argv[])
 {
     rclcpp::init(argc, argv);
+
+    rclcpp::spin(std::make_shared<Talker>());
 
     rclcpp::shutdown();
     return 0;
