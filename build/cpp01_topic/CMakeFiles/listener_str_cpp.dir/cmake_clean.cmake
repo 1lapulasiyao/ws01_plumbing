d070f17a44d7e16f6dd6d3952e17f9a98c15bfb4
1lapulasiyao <513210591@qq.com>
2026-06-28 15:28:38 +0800

    Fifth save

---
+++ build/cpp01_topic/CMakeFiles/listener_str_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/listener_str_cpp.dir/src/listener_str_cpp.cpp.o"
+  "listener_str_cpp"
+  "listener_str_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/listener_str_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- build/cpp01_topic/cmake_install.cmake
+++ build/cpp01_topic/cmake_install.cmake
@@ -58,6 +58,26 @@ if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_
 endif()
 
 if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cpp01_topic/listener_str_cpp" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cpp01_topic/listener_str_cpp")
+    file(RPATH_CHECK
+         FILE "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cpp01_topic/listener_str_cpp"
+         RPATH "")
+  endif()
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/cpp01_topic" TYPE EXECUTABLE FILES "/home/abc/ws01_plumbing/build/cpp01_topic/listener_str_cpp")
+  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cpp01_topic/listener_str_cpp" AND
+     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cpp01_topic/listener_str_cpp")
+    file(RPATH_CHANGE
+         FILE "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cpp01_topic/listener_str_cpp"
+         OLD_RPATH "/opt/ros/galactic/lib:"
+         NEW_RPATH "")
+    if(CMAKE_INSTALL_DO_STRIP)
+      execute_process(COMMAND "/usr/bin/strip" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/lib/cpp01_topic/listener_str_cpp")
+    endif()
+  endif()
+endif()
+
+if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_COMPONENT)
   file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/ament_index/resource_index/package_run_dependencies" TYPE FILE FILES "/home/abc/ws01_plumbing/build/cpp01_topic/ament_cmake_index/share/ament_index/resource_index/package_run_dependencies/cpp01_topic")
 endif()
 
